6db66050f8387f09c47b35676f824231c7d3040c
Nabin-Sapkota <147266054+Nabin-Sapkota@users.noreply.github.com>
2025-08-15 21:50:54 +0545

    dictionaries

---
+++ python/dictionaries.py
@@ -0,0 +1,15 @@
+# mapping member_id to name
+
+member_info = {"M101": "John Doe", "M102": "Jane Smith"}
+
+# access value
+print(member_info["M101"])
+
+# add a new member
+member_info["M103"] = "Alex Page"
+
+# get all keys 
+print(member_info.keys())
+
+# get all values
+print(member_info.values())
